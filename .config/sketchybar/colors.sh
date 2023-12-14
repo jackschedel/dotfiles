118dae66112788852c67321a1f7d480f45a6be3c
Jack Schedel <jackschedel@gmail.com>
2023-12-14 13:58:04 -0500

    tmux new bottom bar + wezterm padding + sketchybar misc (dark hard)

--- .config/sketchybar/colors.sh
+++ .config/sketchybar/colors.sh
@@ -9,9 +9,9 @@ export BLUE=0xff83a598
 export YELLOW=0xfffabd2f
 export ORANGE=0xfffe8019
 export MAGENTA=0xffd3869b
-export GREY=0xff3c3836 #bg1
+export GREY=0xff282828 #bg
 export TRANSPARENT=0x00000000
-export BACKGROUND_MAIN=0xff282828 #bg
+export BACKGROUND_MAIN=0xff1d2021 #bg0_h
 
 # General bar colors
 export BAR_COLOR=$BACKGROUND_MAIN
@@ -19,7 +19,9 @@ export BAR_BORDER_COLOR=$BACKGROUND_MAIN
 export ICON_COLOR=$WHITE
 export LABEL_COLOR=$WHITE
 export BACKGROUND_1=$GREY
-export BACKGROUND_2=0xff665c54
+
+# currently only used for volume slider
+export BACKGROUND_2=0xff665c54 #bg3
 
 export POPUP_BACKGROUND_COLOR=$BACKGROUND_MAIN
 export POPUP_BORDER_COLOR=$GREY
--- .wezterm.lua
+++ .wezterm.lua
@@ -13,6 +13,13 @@ config.font = wezterm.font("JetBrainsMono Nerd Font")
 
 config.font_size = 13
 
+config.window_padding = {
+	left = 0,
+	right = 0,
+	top = 0,
+	bottom = 0,
+}
+
 config.hide_tab_bar_if_only_one_tab = true
 
 config.color_scheme = "Gruvbox dark, medium (base16)"
