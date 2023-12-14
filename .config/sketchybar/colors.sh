d796d437f55e36bc2c3edb7088c31bf8c47b2cfd
Jack Schedel <jackschedel@gmail.com>
2023-12-13 23:05:16 -0500

    yabai + skhdrc + sketchybar

---
+++ .config/sketchybar/colors.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Color Palette
+export BLACK=0xff181926
+export WHITE=0xffebdbb2
+export RED=0xfffb4934
+export GREEN=0xffb8bb26
+export BLUE=0xff83a598
+export YELLOW=0xfffabd2f
+export ORANGE=0xfffe8019
+export MAGENTA=0xffd3869b
+export GREY=0xff3c3836 #bg1
+export TRANSPARENT=0x00000000
+export BACKGROUND_MAIN=0xff282828 #bg
+
+# General bar colors
+export BAR_COLOR=$BACKGROUND_MAIN
+export BAR_BORDER_COLOR=$BACKGROUND_MAIN
+export ICON_COLOR=$WHITE
+export LABEL_COLOR=$WHITE
+export BACKGROUND_1=$GREY
+export BACKGROUND_2=0xff665c54
+
+export POPUP_BACKGROUND_COLOR=$BACKGROUND_MAIN
+export POPUP_BORDER_COLOR=$GREY
+
+export SHADOW_COLOR=$BLACK
