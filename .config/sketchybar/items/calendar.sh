f920ea03fc47430c340101b3eef5f9969f5b8007
Jack Schedel <jackschedel@gmail.com>
2023-12-14 23:08:19 -0500

    no sketchybar zen + 12hr time + yabairc padding

--- .config/sketchybar/items/calendar.sh
+++ .config/sketchybar/items/calendar.sh
@@ -4,12 +4,11 @@ calendar=(
   icon=cal
   icon.font="$FONT:Black:12.0"
   icon.padding_right=0
-  label.width=45
+  label.width=75
   label.align=right
   padding_left=15
   update_freq=30
   script="$PLUGIN_DIR/calendar.sh"
-  click_script="$PLUGIN_DIR/zen.sh"
 )
 
 sketchybar --add item calendar right       \
