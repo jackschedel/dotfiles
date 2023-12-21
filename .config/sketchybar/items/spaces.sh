b3241aec9a4c9e40d222575f938a679a827a1132
Jack Schedel <jackschedel@gmail.com>
2023-12-21 10:35:53 -0500

    sketch to no rounded, non-active space icon fix

--- .config/sketchybar/items/spaces.sh
+++ .config/sketchybar/items/spaces.sh
@@ -20,7 +20,7 @@ do
     padding_right=2
     label.padding_right=20
     icon.highlight_color=$RED
-    label.color=$GREY
+    label.color=$WHITE
     label.highlight_color=$WHITE
     label.font="sketchybar-app-font:Regular:16.0"
     label.y_offset=-1
